15c84fba63d0b6603dfc18f44a7b47577ce005fd
remi gai <remigai@remis-MacBook-Pro.local>
2022-10-21 15:57:27 -0500

    added interface for L1

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# Sample Hardhat Project
+# Getting started
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
+Requires docker.
 
 ```shell
-npx hardhat help
+npx install
+npx hardhat starknet-compile contracts/starknet/contract.cairo
 npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
 ```
--- contracts/ethereum/test.sol
+++ contracts/ethereum/test.sol
@@ -1,10 +1,31 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract test{
-  uint _price;
+/**
+ * @dev Interface for StarkNet core contract, used to consume messages passed from L2 to L1.
+ */
+interface IStarknetCore {
+    /**
+     * @dev Consumes a message that was sent from an L2 contract. Returns the hash of the message.
+     */
+    function consumeMessageFromL2(
+        uint256 fromAddress,
+        uint256[] calldata payload
+    ) external;
+}
 
-  constructor() {
+contract EthereumDepositContract{
+
+  IStarknetCore starknetCore;
+  constructor(IStarknetCore _starknetCore) {
+    starknetCore = _starknetCore;
+  }
+
+  function remoteDepositAccount() external {
+
+  }
+  
+  function remoteWithdrawAccount() external {
 
   }
 }
