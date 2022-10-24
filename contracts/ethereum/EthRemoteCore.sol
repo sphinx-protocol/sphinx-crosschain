fde029c053ba5000ff68016bbf096046453326ab
remi gai <remigai@remis-MacBook-Pro.local>
2022-10-24 18:22:46 -0500

    added blocknumber and nullifier to avoid double withdrawal

--- contracts/ethereum/EthRemoteCore.sol
+++ contracts/ethereum/EthRemoteCore.sol
@@ -23,6 +23,7 @@ contract EthRemoteCore{
   address public owner;
   uint256 public l2StorageProverAddress;
   bool public proverAddressIsSet = false;
+  mapping (bytes32 => bool) public nullifiers;
   IStarknetCore starknetCore;
   
   constructor(IStarknetCore _starknetCore) {
@@ -51,7 +52,7 @@ contract EthRemoteCore{
   }
   
   // Note: this logic assumes that the messaging layer will never fail.
-  function remoteWithdrawAccount(uint256 tokenAddress, uint amount, uint256 userAddress) external {
+  function remoteWithdrawAccount(uint256 tokenAddress, uint amount, uint256 userAddress, uint256 blocknumber) external {
     require(proverAddressIsSet, "No prover");
 
     // Construct the L2 -> L1 withdrawal message payload.
@@ -59,12 +60,17 @@ contract EthRemoteCore{
     payload[0] = userAddress;
     payload[1] = tokenAddress;
     payload[2] = amount;
+    payload[3] = blocknumber;
 
     // Fails if message doesn't exist.
     starknetCore.consumeMessageFromL2(l2StorageProverAddress, payload);
 
     address convertedUserAddress = address(uint160(userAddress));
     address convertedTokendAddress = address(uint160(userAddress));
+
+    // hash of payload is the nullifier to avoid double spending
+    bytes32 nullifier = keccak256(abi.encodePacked(payload));
+    require(!nullifiers[nullifier], "Double spending");
 
     IERC20(convertedTokendAddress).transfer(convertedUserAddress, amount);
   }
