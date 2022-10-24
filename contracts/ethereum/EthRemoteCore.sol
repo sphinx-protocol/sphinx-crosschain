974c90cba536a936801a7b1a4b29209ef0493aed
remi gai <remigai@remis-MacBook-Pro.local>
2022-10-24 17:50:09 -0500

    added remote contracts

--- contracts/ethereum/EthRemoteCore.sol
+++ contracts/ethereum/EthRemoteCore.sol
@@ -14,24 +14,61 @@ interface IStarknetCore {
         uint256 fromAddress,
         uint256[] calldata payload
     ) external;
+
+    function sendMessageToL2(
+        uint256 toAddress,
+        uint256 selector,
+        uint256[] calldata payload
+    ) external returns (bytes32);
 }
 
 contract EthRemoteCore{
+  address public owner;
+  uint256 public l2StorageProverAddress;
+  bool public proverAddressIsSet = false;
   IStarknetCore starknetCore;
-  // user address => token address => amount
-  mapping (address => mapping (address => uint)) public userStakedAmountOfTokens;
+  
   constructor(IStarknetCore _starknetCore) {
     starknetCore = _starknetCore;
+    owner = msg.sender;
   }
 
-  function remoteDepositAccount(address tokenContract, uint amount) external {
-    IERC20(tokenContract).transfer(address(this), amount);
-    userStakedAmountOfTokens[tokenContract][msg.sender] += amount;
+  function setProverAddress(uint256 _l2StorageProverAddress) external {
+    require(msg.sender == owner, "Only owner");
+    l2StorageProverAddress = _l2StorageProverAddress;
+    proverAddressIsSet = true;
+  }
+
+  function remoteDepositAccount(uint256 toAddress, uint256 selector, address tokenAddress, uint256 amount) external {
+    require(proverAddressIsSet, "No prover");
+    IERC20(tokenAddress).transfer(address(this), amount);
+
+    // Construct the L1 -> L2 message payload.
+    uint256[] memory payload = new uint256[](3);
+    payload[0] = uint160(msg.sender);
+    payload[1] = uint160(tokenAddress);
+    payload[2] = amount;
+    
+    starknetCore.sendMessageToL2(toAddress, selector, payload);
   }
   
-  function remoteWithdrawAccount(address tokenContract, uint amount) external {
-    require(userStakedAmountOfTokens[tokenContract][msg.sender] >= amount, "Not enough tokens");
-    IERC20(tokenContract).transfer(address(this), amount);
-    userStakedAmountOfTokens[tokenContract][msg.sender] -= amount;
+  function remoteWithdrawAccount(uint256 tokenAddress, uint amount, uint256 userAddress) external {
+    require(proverAddressIsSet, "No prover");
+
+    // Construct the L2 -> L1 withdrawal message payload.
+    uint256[] memory payload = new uint256[](3);
+    payload[0] = userAddress;
+    payload[1] = tokenAddress;
+    payload[2] = amount;
+
+    // Fails if message doesn't exist.
+    starknetCore.consumeMessageFromL2(l2StorageProverAddress, payload);
+
+    address convertedUserAddress = address(uint160(userAddress));
+    address convertedTokendAddress = address(uint160(userAddress));
+
+    IERC20(convertedTokendAddress).transfer(convertedUserAddress, amount);
   }
 }
+
+// Notes: this logic assumes that the messaging layer will never fail.
