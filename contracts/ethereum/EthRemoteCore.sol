41d47b3b7247f0b94a79d2a8259cb556302fca3e
yeungparkhay <yeungparkhay@gmail.com>
2022-10-29 11:46:13 +0100

    Renamed files and integrated remote deposit

--- contracts/ethereum/EthRemoteCore.sol
+++ contracts/ethereum/EthRemoteCore.sol
@@ -19,69 +19,83 @@ interface IStarknetCore {
     ) external payable returns (bytes32);
 }
 
-contract EthRemoteCore{
-  address public owner;
-  bool public proverAddressIsSet = false;
-  uint256 public l2StorageProverAddress;
-  uint256 public nonce;
-  uint256 public SUBMIT_L1_BLOCKHASH_SELECTOR = 598342674068027518481179578557554850038206119856216505601406522348670006916;
-  mapping (bytes32 => bool) public nullifiers;
-  IStarknetCore public starknetCore;
-  
-  constructor(IStarknetCore _starknetCore) {
-    starknetCore = _starknetCore;
-    owner = msg.sender;
-    nonce = 0;
-  }
+contract EthRemoteCore {
+    address public owner;
+    bool public remoteAddressIsSet = false;
+    uint256 public l2EthRemoteCoreAddress;
+    uint256 public nonce;
 
-  function setProverAddress(uint256 _l2StorageProverAddress) external {
-    require(msg.sender == owner, "Only owner");
-    l2StorageProverAddress = _l2StorageProverAddress;
-    proverAddressIsSet = true;
-  }
+    /// starknetSelector("remote_deposit")
+    uint256 public REMOTE_DEPOSIT_SELECTOR =
+        1795963806397751995885658948754777263288166063579995395494415972176756934361;
+    mapping(bytes32 => bool) public nullifiers;
+    IStarknetCore public starknetCore;
 
-  // Note: this logic assumes that the messaging layer will never fail.
-  // https://www.cairo-lang.org/docs/hello_starknet/l1l2.html
-  function remoteDepositAccount(address tokenAddress, uint256 amount) external payable {
-    require(proverAddressIsSet, "No prover");
-    IERC20(tokenAddress).transfer(address(this), amount); 
+    constructor(IStarknetCore _starknetCore) {
+        starknetCore = _starknetCore;
+        owner = msg.sender;
+        nonce = 0;
+    }
 
-    // Construct the L1 -> L2 message payload.
-    uint256[] memory payload = new uint256[](4);
-    payload[0] = uint160(msg.sender);
-    payload[1] = uint160(tokenAddress);
-    payload[2] = amount;
-    payload[3] = nonce;
+    function setRemoteAddress(uint256 _l2EthRemoteCoreAddress) external {
+        require(msg.sender == owner, "Only owner");
+        l2EthRemoteCoreAddress = _l2EthRemoteCoreAddress;
+        remoteAddressIsSet = true;
+    }
 
-    nonce++;
+    // Note: this logic assumes that the messaging layer will never fail.
+    // https://www.cairo-lang.org/docs/hello_starknet/l1l2.html
+    function remoteDepositAccount(address tokenAddress, uint256 amount)
+        external
+        payable
+    {
+        require(remoteAddressIsSet, "No prover");
+        IERC20(tokenAddress).transfer(address(this), amount);
 
-    // Pass in a message fee. 
-    /// starknetSelector(receive_from_l1) 598342674068027518481179578557554850038206119856216505601406522348670006916
-    starknetCore.sendMessageToL2{value: msg.value}(l2StorageProverAddress, SUBMIT_L1_BLOCKHASH_SELECTOR, payload);
-  }
-  
-  // Note: this logic assumes that the messaging layer will never fail.
-  function remoteWithdrawAccount(uint256 tokenAddress, uint amount, uint256 userAddress, uint256 nonce) external {
-    require(proverAddressIsSet, "No prover");
+        // Construct the L1 -> L2 message payload.
+        uint256[] memory payload = new uint256[](4);
+        payload[0] = uint160(msg.sender);
+        payload[1] = uint160(tokenAddress);
+        payload[2] = amount;
+        payload[3] = nonce;
 
-    // Construct the L2 -> L1 withdrawal message payload.
-    uint256[] memory payload = new uint256[](4);
-    payload[0] = userAddress;
-    payload[1] = tokenAddress;
-    payload[2] = amount;
-    payload[3] = nonce;
+        nonce++;
 
-    // Fails if message doesn't exist.
-    starknetCore.consumeMessageFromL2(l2StorageProverAddress, payload);
+        // Pass in a message fee.
+        starknetCore.sendMessageToL2{value: msg.value}(
+            l2EthRemoteCoreAddress,
+            REMOTE_DEPOSIT_SELECTOR,
+            payload
+        );
+    }
 
-    address convertedUserAddress = address(uint160(userAddress));
-    address convertedTokendAddress = address(uint160(userAddress));
+    // Note: this logic assumes that the messaging layer will never fail.
+    function remoteWithdrawAccount(
+        uint256 tokenAddress,
+        uint256 amount,
+        uint256 userAddress,
+        uint256 nonce
+    ) external {
+        require(remoteAddressIsSet, "No prover");
 
-    // hash of payload is the nullifier to avoid double spending
-    bytes32 nullifier = keccak256(abi.encodePacked(payload));
-    require(!nullifiers[nullifier], "Double spend");
+        // Construct the L2 -> L1 withdrawal message payload.
+        uint256[] memory payload = new uint256[](4);
+        payload[0] = userAddress;
+        payload[1] = tokenAddress;
+        payload[2] = amount;
+        payload[3] = nonce;
 
-    IERC20(convertedTokendAddress).transfer(convertedUserAddress, amount);
-    nullifiers[nullifier] = true;
-  }
+        // Fails if message doesn't exist.
+        starknetCore.consumeMessageFromL2(l2EthRemoteCoreAddress, payload);
+
+        address convertedUserAddress = address(uint160(userAddress));
+        address convertedTokendAddress = address(uint160(userAddress));
+
+        // hash of payload is the nullifier to avoid double spending
+        bytes32 nullifier = keccak256(abi.encodePacked(payload));
+        require(!nullifiers[nullifier], "Double spend");
+
+        IERC20(convertedTokendAddress).transfer(convertedUserAddress, amount);
+        nullifiers[nullifier] = true;
+    }
 }
