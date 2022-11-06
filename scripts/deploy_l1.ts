320c612be6cbcc8f2c90d064c7e8f1578daccab3
yeungparkhay <yeungparkhay@gmail.com>
2022-11-06 22:44:08 +0800

    asdf

--- scripts/deploy_l1.ts
+++ scripts/deploy_l1.ts
@@ -1,7 +1,7 @@
 // To deploy: npx hardhat run --network goerli scripts/deploy_l1.ts
-// To verify: npx hardhat verify --network goerli 0xF7D18E41638cC1790999Fc390B72c8b0A574602d "0xa4eD3aD27c294565cB0DCc993BDdCC75432D498c"
+// To verify: npx hardhat verify --network goerli 0xa4B1A83E567cC12f17518497D6EFC63dCbE9f0e5 "0xa4eD3aD27c294565cB0DCc993BDdCC75432D498c"
 
-// Deployment address: 0xF7D18E41638cC1790999Fc390B72c8b0A574602d
+// Deployment address: 0xa4B1A83E567cC12f17518497D6EFC63dCbE9f0e5
 
 import { ethers } from "hardhat"
 
